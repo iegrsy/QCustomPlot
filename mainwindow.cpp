5def3de9506761fb34e5076c1a28813f90358e55
iegrsy <iegursoy28@gmail.com>
2017-08-09 14:25:23 +0300

    toplu kıyım:)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,28 +6,18 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->customPlot->installEventFilter(this);
-    ui->frame_3->installEventFilter(this);
-
-    this->setContextMenuPolicy(Qt::CustomContextMenu);
-    connect(this, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(ShowContextMenu(const QPoint &)));
-
-    QVector<QCPGraphData> randState;
-    for (int var = 0; var < 250; ++var) {
-        if( (50 < var && var < 130) || (170 < var && var < 230) )
-        {
-            randState.append(QCPGraphData(var,var*2));
-        }
-        else
-        {
-            randState.append(QCPGraphData(var,0));
-        }
-    }
-
-    plotGraphData(randState);
-
-
     setMouseTracking(true);
+
+    ui->customPlot->installEventFilter(this);
+
+    textItem = new QCPItemText(ui->customPlot);
+    this->setContextMenuPolicy(Qt::CustomContextMenu);
+
+    connect(ui->customPlot, &QCustomPlot::mouseMove, this, &MainWindow::onMouseMove);
+
+    plotGraphData(randomDataCreate());
+    //    plotGraph();
+
 }
 
 MainWindow::~MainWindow()
@@ -37,30 +27,50 @@ MainWindow::~MainWindow()
 
 void MainWindow::plotGraph()
 {
-    ui->customPlot->setLocale(QLocale(QLocale::Turkish, QLocale::Turkey));
-
+    // add two new graphs and set their look:
     ui->customPlot->addGraph();
-    QColor color(255,150, 150, 150);
-    ui->customPlot->graph()->setLineStyle(QCPGraph::lsLine);
-    ui->customPlot->graph()->setPen(QPen(color.lighter(200)));
-    ui->customPlot->graph()->setBrush(QBrush(color));
+    ui->customPlot->graph(0)->setPen(QPen(Qt::blue)); // line color blue for first graph
+    ui->customPlot->graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20))); // first graph will be filled with translucent blue
+    ui->customPlot->addGraph();
+    ui->customPlot->graph(1)->setPen(QPen(Qt::red)); // line color red for second graph
 
-    QSharedPointer<QCPAxisTicker> stateTicker(new QCPAxisTicker);
-    ui->customPlot->xAxis->setTicker(stateTicker);
-    ui->customPlot->yAxis->setTicker(stateTicker);
+    // generate some points of data (y0 for first, y1 for second graph):
+    QVector<double> x(251), y0(251), y1(251);
+    for (int i=0; i<251; ++i)
+    {
+        x[i] = i;
+        y0[i] = qExp(-i/150.0)*qCos(i/10.0); // exponentially decaying cosine
+        y1[i] = qExp(-i/150.0);              // exponential envelope
+    }
 
+    // configure right and top axis to show ticks but no labels:
+    // (see QCPAxisRect::setupFullAxesBox for a quicker method to do this)
     ui->customPlot->xAxis2->setVisible(true);
-    ui->customPlot->yAxis2->setVisible(true);
-    ui->customPlot->xAxis2->setTicks(false);
-    ui->customPlot->yAxis2->setTicks(false);
     ui->customPlot->xAxis2->setTickLabels(false);
+    ui->customPlot->yAxis2->setVisible(true);
     ui->customPlot->yAxis2->setTickLabels(false);
 
-    ui->customPlot->xAxis->setRange(0, 10);
-    ui->customPlot->yAxis->setRange(0, 1);
+    // make left and bottom axes always transfer their ranges to right and top axes:
+    connect(ui->customPlot->xAxis, SIGNAL(rangeChanged(QCPRange)), ui->customPlot->xAxis2, SLOT(setRange(QCPRange)));
+    //    connect(ui->customPlot->yAxis, SIGNAL(rangeChanged(QCPRange)), ui->customPlot->yAxis2, SLOT(setRange(QCPRange)));
+
+    // pass data points to graphs:
+    ui->customPlot->graph(0)->setData(x, y0);
+    ui->customPlot->graph(1)->setData(x, y1);
+
+    // let the ranges scale themselves so graph 0 fits perfectly in the visible area:
+    ui->customPlot->graph(0)->rescaleAxes();
+
+    // same thing for graph 1, but only enlarge ranges (in case graph 1 is smaller than graph 0):
+    ui->customPlot->graph(1)->rescaleAxes(true);
+
+    // Note: we could have also just called ui->customPlot->rescaleAxes(); instead
+    // Allow user to drag axis ranges with mouse, zoom with mouse wheel and select graphs by clicking:
+    ui->customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
 
     ui->customPlot->legend->setVisible(true);
     ui->customPlot->legend->setBrush(QColor(255, 255, 255, 150));
+
 }
 
 void MainWindow::plotGraphData(QVector<QCPGraphData> dataList)
@@ -84,21 +94,13 @@ void MainWindow::plotGraphData(QVector<QCPGraphData> dataList)
     ui->customPlot->xAxis->setLabel("Value");
     ui->customPlot->yAxis->setLabel("Count");
 
-    min_xAxis = dataList.first().key;
-    max_xAxis = dataList.last().key;
-
-    lower_xAxis = min_xAxis;
-    upper_xAxis = lower_xAxis + displayRange;
-
-    lower_yAxis = 0.0;
-    upper_yAxis = displayRangeY;
-
-    ui->customPlot->xAxis->setRange(lower_xAxis , upper_xAxis);
-    ui->customPlot->yAxis->setRange(lower_yAxis , upper_yAxis);
+    ui->customPlot->graph(0)->rescaleAxes();
 
     ui->customPlot->legend->setVisible(true);
     ui->customPlot->legend->setBrush(QColor(255, 255, 255, 150));
     ui->customPlot->axisRect()->insetLayout()->setInsetAlignment(0, Qt::AlignRight|Qt::AlignBottom);
+    ui->customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables | QCP::iSelectItems);
+
 }
 
 bool MainWindow::eventFilter(QObject *obj, QEvent *event)
@@ -109,110 +111,56 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *event)
         if (event->type() == QEvent::KeyPress)
         {
             QKeyEvent *keyEvent = static_cast<QKeyEvent*>(event);
-
             if(keyEvent->key() == Qt::Key_Control)
                 ctrlKeyDown = true;
-
-            return true;
+            return false;
         }
         else if (event->type() == QEvent::KeyRelease)
         {
             QKeyEvent *keyEvent = static_cast<QKeyEvent*>(event);
-
             if(keyEvent->key() == Qt::Key_Control)
                 ctrlKeyDown = false;
-
-            return true;
+            return false;
         }
 
         //Capture wheel event
         if ( event->type() == QEvent::Wheel )
         {
-            QWheelEvent *wheelEvent = static_cast<QWheelEvent*>(event);
-            int zoom = wheelEvent->delta();
-
-            if (zoom > 0)
+            if(ctrlKeyDown)
             {
-                if(ctrlKeyDown)
-                {
-                    displayRangeSetY(mouseWheelStep);
-                    ui->customPlot->yAxis->setRange(lower_yAxis, upper_yAxis );
-                    ui->customPlot->replot();
-                }
-                else
-                {
-                    displayRangeSet(mouseWheelStep);
-                    ui->customPlot->xAxis->setRange(lower_xAxis, upper_xAxis );
-                    ui->customPlot->replot();
-                }
+                ui->customPlot->axisRect()->setRangeZoomAxes(ui->customPlot->xAxis,ui->customPlot->yAxis);
+                ui->customPlot->axisRect()->setRangeZoom(ui->customPlot->yAxis->orientation());
             }
             else
             {
-                if(ctrlKeyDown)
-                {
-                    displayRangeSetY(-mouseWheelStep);
-                    ui->customPlot->yAxis->setRange(lower_yAxis, upper_yAxis );
-                    ui->customPlot->replot();
-                }
-                else
-                {
-                    displayRangeSet(-mouseWheelStep);
-                    ui->customPlot->xAxis->setRange(lower_xAxis, upper_xAxis );
-                    ui->customPlot->replot();
-                }
+                ui->customPlot->axisRect()->setRangeZoomAxes(ui->customPlot->xAxis,ui->customPlot->yAxis);
+                ui->customPlot->axisRect()->setRangeZoom(ui->customPlot->xAxis->orientation());
             }
-            return true;
+            return false;
         }
 
         //Capture mouse event
-        if((event->type() == QEvent::MouseButtonPress) && Qt::LeftButton)
+        if(event->type() == QEvent::MouseButtonPress)
         {
             QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
 
-            lastPoint = QPoint(mouseEvent->globalX(),mouseEvent->globalY());
-            mouseDown = true;
-
-            return true;
-        }
-        else if ( event->type() == QEvent::MouseButtonRelease)
-        {
-            //QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
-
-            lower_xAxis = lower_xAxis + difx;
-            upper_xAxis = upper_xAxis + difx;
-
-            lower_yAxis = lower_yAxis + dify;
-            upper_yAxis = upper_yAxis + dify;
-
-            mouseDown = false;
-
-            return true;
-        }
-        else if ( event->type() == QEvent::MouseMove )
-        {
-            if(mouseDown == true)
+            if(mouseEvent->button() == Qt::LeftButton)
             {
-                QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
-
-                difx = (lastPoint.x() - mouseEvent->globalX())/5;
-                dify = (lastPoint.y() - mouseEvent->globalY());
-
-                ui->customPlot->xAxis->setRange(lower_xAxis + difx, upper_xAxis + difx);
-                ui->customPlot->yAxis->setRange(lower_yAxis + dify, upper_yAxis + dify);
-                ui->customPlot->replot();
-
-                return true;
+                mouseDown = true;
+                mouseClickData(mouseEvent, obj);
             }
+            else if (mouseEvent->button() == Qt::RightButton)
+            {
+                ShowContextMenu(mouseEvent->globalPos());
+            }
+
+
+            return false;
         }
         else if ( event->type() == QEvent::MouseButtonDblClick)
         {
             qDebug()<<"laaaaan";
-            return true;
-        }
-        else if (event->type() == QEvent::MouseButtonPress && Qt::RightButton)
-        {
-            qDebug()<<"hadi ama";
-            return true;
+            return false;
         }
         else
         {
@@ -226,7 +174,7 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *event)
         {
             QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
 
-            qDebug()<<"burası frame0";
+            qDebug()<<mouseEvent;
             return true;
         }
     }
@@ -237,34 +185,20 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *event)
     }
 }
 
-void MainWindow::displayRangeSet(double range)
-{
-    displayRange += 2*range;
-    lower_xAxis -= range;
-    upper_xAxis = lower_xAxis + displayRange;
-}
-
-void MainWindow::displayRangeSetY(double range)
-{
-    displayRangeY += 2*range;
-    lower_yAxis -= range;
-    upper_yAxis = lower_xAxis + displayRange;
-}
-
 void MainWindow::ShowContextMenu(const QPoint &pos)
 {
-   QMenu contextMenu(tr("Context menu"), this);
+    QMenu contextMenu(tr("Context menu"), this);
 
-   QAction action1("item1", this);
-   QAction action2("item2", this);
+    QAction action1("item1", this);
+    QAction action2("item2", this);
 
-   connect(&action1, SIGNAL(triggered()), this, SLOT(myContextMenuSlot1()));
-   connect(&action2, SIGNAL(triggered()), this, SLOT(myContextMenuSlot2()));
+    connect(&action1, SIGNAL(triggered()), this, SLOT(myContextMenuSlot1()));
+    connect(&action2, SIGNAL(triggered()), this, SLOT(myContextMenuSlot2()));
 
-   contextMenu.addAction(&action1);
-   contextMenu.addAction(&action2);
+    contextMenu.addAction(&action1);
+    contextMenu.addAction(&action2);
 
-   contextMenu.exec(mapToGlobal(pos));
+    contextMenu.exec(pos);
 }
 
 void MainWindow::myContextMenuSlot1()
@@ -276,6 +210,49 @@ void MainWindow::myContextMenuSlot2()
 {
     qDebug()<<"slot2 trig";
 }
+
+void MainWindow::onMouseMove(QMouseEvent *event)
+{
+    QCustomPlot* customPlot = qobject_cast<QCustomPlot*>(sender());
+    double x = customPlot->xAxis->pixelToCoord(event->pos().x());
+    double y = customPlot->yAxis->pixelToCoord(event->pos().y());
+    textItem->setText(QString("(%1, %2)\n").arg((int)x).arg((int)y));
+    textItem->position->setCoords(QPointF(x, (y)));
+    textItem->setFont(QFont(font().family(), 10));
+    customPlot->replot();
+}
+
+QVector<QCPGraphData> MainWindow::randomDataCreate(){
+    QVector<QCPGraphData> randState;
+    for (int var = 0; var < 10000; ++var) {
+        if( (50 < var && var < 130) || (170 < var && var < 230) )
+        {
+            randState.append(QCPGraphData(var,50 + rand()%5));
+        }
+        else
+        {
+            randState.append(QCPGraphData(var,5 + rand()%3));
+        }
+    }
+    return randState;
+}
+
+
+
+void MainWindow::mouseClickData(QMouseEvent *event, QObject *obj)
+{
+    QCustomPlot* customPlot = qobject_cast<QCustomPlot*>(obj);
+    double x = customPlot->xAxis->pixelToCoord(event->pos().x());
+    double dataValue = customPlot->plottable()->interface1D()->dataMainValue(x);
+
+    ui->lblData->setText(QString::number(dataValue));
+    ui->customPlot->setMouseTracking(true);
+}
+
+
+
+
+
 
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -32,30 +32,26 @@ public:
     void plotGraphData(QVector<QCPGraphData> dataList);
 
     //pan veriable
-    double displayRange = 100;
-    double displayRangeY = 10;
-    double mouseWheelStep = 3.0;
+
     bool mouseDown = false;
     bool ctrlKeyDown =  false;
-    QPoint lastPoint;
-    double difx;
-    double dify;
-    double lower_xAxis,upper_xAxis,lower_yAxis,upper_yAxis;
-    double min_xAxis, max_xAxis;
-    void displayRangeSet(double range);
-    void displayRangeSetY(double range);
 
+
+    QCPItemText *textItem;
+    QVector<QCPGraphData> randomDataCreate();
+
+    void mouseClickData(QMouseEvent *event, QObject *obj);
 public slots:
-    void ShowContextMenu(const QPoint &pos);
 private:
     Ui::MainWindow *ui;
 protected:
     bool eventFilter(QObject *obj, QEvent *ev);
-
 protected slots:
 private slots:
+    void ShowContextMenu(const QPoint &pos);
     void myContextMenuSlot1();
     void myContextMenuSlot2();
+    void onMouseMove(QMouseEvent *event);
 };
 
 #endif // MAINWINDOW_H
